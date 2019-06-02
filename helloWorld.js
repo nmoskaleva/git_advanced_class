e1a2955a039827ae87d71e30fa19baae6238d298
nmoskaleva <nsmoskaleva@gmail.com>
2019-06-02 15:27:29 +0200

    Added goodbye world function

--- helloWorld.js
+++ helloWorld.js
@@ -1,5 +1,17 @@
 const helloWorld = function() {
-  return 'Hello world!';
+  return "Hello world!";
 };
 
 console.log(helloWorld());
+
+const helloWorld = function() {
+  return "Hello world!";
+};
+
+console.log(helloWorld());
+
+const goodbyeWorld = function() {
+  return "Bye world!";
+};
+
+console.log(goodbyeWorld());
